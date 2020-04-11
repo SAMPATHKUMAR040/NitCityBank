8a8e449b7312cccbe7a181fe7c1beb3dd80f1264
kumar <sampathkumar22255@gmail.com>
2020-04-11 21:58:33 +0530

    test.java modified

--- DemoProj1/src/com/nt/test/Test.java
+++ DemoProj1/src/com/nt/test/Test.java
@@ -1,8 +1,12 @@
 package com.nt.test;
 
+import com.nt.beans.Employee;
+
 public class Test {
 	public static void main(String[] args) {
 		
+		Employee emp=new Employee();
+		System.out.println(emp.getSysDate());
 	}
 
 }
